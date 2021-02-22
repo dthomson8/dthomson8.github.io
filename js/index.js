08e129e9c50804c7dc3dbe9f0d8aa0094838780b
dthomson8 <dthomson8@gatech.edu>
2021-02-22 12:40:34 -0500

    adding all files

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function showLinks() {
+  document.getElementById('plus').style.display = "none"
+  document.getElementById('link').style.display = "block"
+}
+
+
+function hideLinks() {
+  document.getElementById('plus').style.display = "block"
+  document.getElementById('link').style.display = "none"
+}
